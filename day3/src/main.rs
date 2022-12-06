19da86a127546685469def56ec465e0e5b951271
EoghanMulvenna <eoghan.mulvenna@hyperluminalgames.com>
2022-12-06 17:08:35 +0000

    Updates to day 3 part 2 solution

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,5 @@
+use std::char;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{prelude::*, BufReader, Lines};
 
@@ -8,18 +10,29 @@ fn main()
 {
     let lines = load_file("input.txt");
 
-    let mut score = 0;
+    let mut score1 = 0;
+    let mut score2 = 0;
     let mut line_count = 0;
-    let mut line_arary = &[String; 3];
+    let mut line_arary: [String; 3] = Default::default();
+
     for line in lines.into_iter() 
     {
-        score += parse_line(line.unwrap());
-        line_arary[line_count] = &line;
+        let line_string = line.unwrap();
+        line_arary[line_count] = line_string.clone();
+        score1 += parse_line(line_string);
+
 
         line_count+=1;
+
+        if(line_count >= 3)
+        {
+            line_count = 0;
+            score2 += get_score_of_unique(&line_arary);
+        }
     }
 
-    print!("Part1 Score = {}", score)
+    print!("Part1 Score = {}", score1);
+    print!("Part2 Score = {}", score2);
 }
 
 fn load_file(file_path: &str) -> Lines<BufReader<File>>
@@ -43,26 +56,36 @@ fn parse_line(line: String) -> u32
 fn get_score_of_unique(strings: &[String]) -> u32
 {
     let string1 = &strings[0];
-    let mut charVec: Vec<char> = string1.chars().collect();
+    let mut char_map = HashMap::new();
 
-    for string in strings[1..].iter() 
+    for c in string1.chars()
     {
-        let mut i = charVec.len(); 
-        while i > 0 
-        {
-            i -= 1;
-            let character = charVec.get(i).unwrap();
+        char_map.insert(c, false);
+    }
 
-            if(!string.contains(*character))
+    for string in strings[..].iter() 
+    {
+        for character in string.chars().into_iter()
+        {
+            let mapCharacter = &character.clone();
+            if char_map.contains_key(mapCharacter)
             {
-                charVec.remove(i);     
+                char_map.get_mut(&character) = true;
+            }
+        }
+
+        for mapChar in char_map
+        {
+            if mapChar.1
+            {
+                char_map.remove(&mapChar.0);
             }
         }
     }
 
     let mut score = 0;
 
-    for char in charVec
+    for char in char_map.keys()
     {
         let offset: u32 = match char.is_uppercase() 
         {
@@ -70,7 +93,7 @@ fn get_score_of_unique(strings: &[String]) -> u32
             false => LOWER_OFFSET
         };
 
-        score += (char as u32) - offset;
+        score += (*char as u32) - offset;
     }
    
     score
